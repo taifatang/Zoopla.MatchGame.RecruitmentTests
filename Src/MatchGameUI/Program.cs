7f969315954f939e82e6eba7076af660f2bb5a4e
Tai Fa Tang <taifatang@hotmail.com>
2021-02-13 14:54:03 +0000

    breakdown requirements into digestaable tasks

--- Src/MatchGameUI/Program.cs
+++ Src/MatchGameUI/Program.cs
@@ -4,6 +4,21 @@ namespace MatchGameUI
 {
     class Program
     {
+        //Task 1 - Build Basic Game Engine with a simple match rule
+        //Task 2 - Shuffle decks and ability to use multiple decks
+        //Task 3 - Introduce more game modes
+        //Task 4 - Randomise bots reaction time
+
+        /*
+         * GameFlow:
+         * Player specify number of decks and match rule
+         * Game engine shuffle number of requested deck
+         * Game engine determine player A or B wins at random
+         * Winner takes the card drawn
+         * Game resume
+         * Player with most cards win, draw is possible
+         */
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
